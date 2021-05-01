f27290141a82a4c68f83ef760c90ef1079fc295d
Ankur P Chauhan <ankur.chauhan@auth0.com>
2021-05-01 23:47:32 +0200

    ggRefactor and quick cleanup

--- code.ts
+++ code.ts
@@ -1,85 +1,67 @@
 const selection = figma.currentPage.selection;
 
-for (const node of selection as any) {
-  let x = node.findAll((node) => node.x);
-  let y = node.findAll((node) => node.y);
-  let n = node.findAll((node) => node.width && node.height);
-  let radius = node.findAll((node) => node.cornerRadius > 0);
-  let stroke = node.findAll((node) => node.strokeWeight > 0);
+figma.showUI(__html__, { width: 320, height: 320 });
 
-  // 2. Show Plugin UI
-  figma.showUI(__html__, { width: 240, height: 400 });
+if (selection) {
+  for (const node of selection as any) {
+    figma.ui.postMessage({
+      objectWidth: node.width,
+      objectHeight: node.height,
+      percentage: 100,
+    });
 
-  // 3. Send data to plugin UI
-  figma.ui.postMessage({
-    objectWidth: node.width,
-    objectHeight: node.height,
-    objectStroke: node.strokeWeight,
-    objectRadius: node.cornerRadius,
-  });
+    figma.ui.onmessage = async (msg) => {
+      if (msg.type === "resize-by-percentage") {
+        node.rescale(msg.percentageRatio);
+        figma.notify(`⚡️ Hurray! Your selection has been resized by the percentage.`);
+      } else if (msg.type === "resize-by-width") {
+        node.rescale(msg.widthRatio);
+        figma.notify(`⚡️ Hurray! Your selection has been resized by the width.`);
+      } else if (msg.type === "resize-by-height") {
+        node.rescale(msg.heightRatio);
+        figma.notify(`⚡️ Hurray! Your selection has been resized by the height.`);
+      } else {
+        figma.closePlugin();
+      }
+    };
+  }
 
-  // 4. Receive data from plugin UI
-  figma.ui.onmessage = async (msg) => {
-    if (msg.type == "resize-by-percentage") {
-      node.resize(Math.round(msg.width * msg.percentageRatio), Math.round(msg.height * msg.percentageRatio));
-      x.forEach((item) => {
-        item.x = Math.round(item.x * msg.percentageRatio);
-      });
-      y.forEach((item) => {
-        item.y = Math.round(item.y * msg.percentageRatio);
-      });
-      n.forEach((item) => {
-        item.resize(Math.round(item.width * msg.percentageRatio), Math.round(item.height * msg.percentageRatio));
-      });
-      node.cornerRadius = Math.round(node.cornerRadius * msg.percentageRatio);
-      radius.forEach((item) => {
-        item.cornerRadius = Math.round(item.cornerRadius * msg.percentageRatio);
-      });
-      node.strokeWeight = Math.round(node.strokeWeight * msg.percentageRatio);
-      stroke.forEach((item) => {
-        item.strokeWeight = Math.round(item.strokeWeight * msg.percentageRatio);
-      });
-      figma.notify(`⚡️ Hurray! Your selection has been resized by percentage.`);
-    } else if (msg.type == "resize-by-width") {
-      node.resize(Math.round(node.width * msg.widthRatio), Math.round(node.height * msg.widthRatio));
-      x.forEach((item) => {
-        item.x = item.x * msg.widthRatio;
-      });
-      y.forEach((item) => {
-        item.y = item.y * msg.widthRatio;
-      });
-      n.forEach((item) => {
-        item.resize(Math.round(item.width * msg.widthRatio), Math.round(item.height * msg.widthRatio));
-      });
-      node.cornerRadius = Math.round(node.cornerRadius * msg.widthRatio);
-      radius.forEach((item) => {
-        item.cornerRadius = Math.round(item.cornerRadius * msg.widthRatio);
-      });
-      node.strokeWeight = Math.round(node.strokeWeight * msg.widthRatio);
-      stroke.forEach((item) => {
-        item.strokeWeight = Math.round(item.strokeWeight * msg.widthRatio);
-      });
-    } else if (msg.type == "resize-by-height") {
-      node.resize(Math.round(node.width * msg.heightRatio), Math.round(node.height * msg.heightRatio));
-      x.forEach((item) => {
-        item.x = item.x * msg.heightRatio;
-      });
-      y.forEach((item) => {
-        item.y = item.y * msg.heightRatio;
-      });
-      n.forEach((item) => {
-        item.resize(Math.round(item.width * msg.heightRatio), Math.round(item.height * msg.heightRatio));
-      });
-      node.cornerRadius = Math.round(node.cornerRadius * msg.heightRatio);
-      radius.forEach((item) => {
-        item.cornerRadius = Math.round(item.cornerRadius * msg.heightRatio);
-      });
-      node.strokeWeight = Math.round(node.strokeWeight * msg.heightRatio);
-      stroke.forEach((item) => {
-        item.strokeWeight = Math.round(item.strokeWeight * msg.heightRatio);
-      });
+  figma.on("selectionchange", () => {
+    const selection = figma.currentPage.selection;
+    if (selection.length > 0) {
+      figma.notify(`${selection[0].name} selected`, { timeout: 100 });
+      for (const node of selection as any) {
+        figma.ui.postMessage({
+          objectWidth: node.width,
+          objectHeight: node.height,
+          percentage: 100,
+        });
+
+        figma.ui.onmessage = async (msg) => {
+          if (msg.type === "resize-by-percentage") {
+            node.rescale(msg.percentageRatio);
+            figma.notify(`⚡️ Hurray! Your selection has been resized by the percentage.`);
+          } else if (msg.type === "resize-by-width") {
+            node.rescale(msg.widthRatio);
+            figma.notify(`⚡️ Hurray! Your selection has been resized by the width.`);
+          } else if (msg.type === "resize-by-height") {
+            node.rescale(msg.heightRatio);
+            figma.notify(`⚡️ Hurray! Your selection has been resized by the height.`);
+          } else {
+            figma.closePlugin();
+          }
+        };
+      }
     } else {
-      figma.closePlugin();
+      figma.notify(`😭 Sorry! You aren't actively selecting a layer.`, { timeout: 100 });
+
+      figma.ui.postMessage({
+        objectWidth: undefined,
+        objectHeight: undefined,
+        percentage: undefined,
+      });
     }
-  };
+  });
 }
+
+figma.notify(`Please select a layer to get started.`, { timeout: 2000 });
