3a1751a5c51e02905f1bf1e8aebf3f45da44322b
Ankur Chauhan <ankur.chauhan@auth0.com>
2021-08-13 16:43:10 +0200

    Allow scaling multiple items & add typings

--- code.js
+++ code.js
@@ -7,70 +7,120 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+// define selection
 const selection = figma.currentPage.selection;
+// show ui in figma
 figma.showUI(__html__, { width: 320, height: 320 });
+// first check if there is an active selection
 if (selection) {
+    // // traverse selection to each node
     for (const node of selection) {
+        // send selection parameters to UI layer
         figma.ui.postMessage({
             objectWidth: node.width,
             objectHeight: node.height,
             percentage: 100,
         });
+        // receive user input from UI layer
         figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
             if (msg.type === "resize-by-percentage") {
-                node.rescale(msg.percentageRatio);
+                // scale each node based on percentage input
+                selection.forEach((item) => item.rescale(msg.percentageRatio));
+                // alert user on success
                 figma.notify(`⚡️ Hurray! Your selection has been resized by the percentage.`);
             }
             else if (msg.type === "resize-by-width") {
-                node.rescale(msg.widthRatio);
+                // scale each node based on width input
+                selection.forEach((item) => item.rescale(msg.widthRatio));
+                // alert user on success
                 figma.notify(`⚡️ Hurray! Your selection has been resized by the width.`);
             }
             else if (msg.type === "resize-by-height") {
-                node.rescale(msg.heightRatio);
+                // scale each node based on height input
+                selection.forEach((item) => item.rescale(msg.heightRatio));
+                // alert user on success
                 figma.notify(`⚡️ Hurray! Your selection has been resized by the height.`);
             }
-            else {
-                figma.closePlugin();
-            }
         });
+        if (selection.length > 1) {
+            figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
+                if (msg.type === "resize-by-percentage") {
+                    // scale each node based on percentage input
+                    selection.forEach((item) => item.rescale(msg.percentageRatio));
+                    // alert user on success
+                    figma.notify(`⚡️ Hurray! Your selection has been resized by the percentage.`);
+                }
+                else if (msg.type === "resize-by-width") {
+                    // scale each node based on width input
+                    selection.forEach((item) => item.rescale(msg.widthRatio));
+                    // alert user on success
+                    figma.notify(`⚡️ Hurray! Your selection has been resized by the width.`);
+                }
+                else if (msg.type === "resize-by-height") {
+                    // scale each node based on height input
+                    selection.forEach((item) => item.rescale(msg.heightRatio));
+                    // alert user on success
+                    figma.notify(`⚡️ Hurray! Your selection has been resized by the height.`);
+                }
+            });
+        }
     }
+    // check for selection changes while plugin is running
     figma.on("selectionchange", () => {
         const selection = figma.currentPage.selection;
         if (selection.length > 0) {
-            figma.notify(`${selection[0].name} selected`, { timeout: 100 });
+            selection.length > 1
+                ? figma.notify(`${selection.length} layers selected`, { timeout: 100 })
+                : figma.notify(`${selection[0].name} selected`, { timeout: 100 });
             for (const node of selection) {
                 figma.ui.postMessage({
                     objectWidth: node.width,
                     objectHeight: node.height,
                     percentage: 100,
                 });
+                // alert user on success
                 figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
                     if (msg.type === "resize-by-percentage") {
-                        node.rescale(msg.percentageRatio);
+                        selection.forEach((item) => item.rescale(msg.percentageRatio));
                         figma.notify(`⚡️ Hurray! Your selection has been resized by the percentage.`);
                     }
                     else if (msg.type === "resize-by-width") {
-                        node.rescale(msg.widthRatio);
+                        selection.forEach((item) => item.rescale(msg.widthRatio));
                         figma.notify(`⚡️ Hurray! Your selection has been resized by the width.`);
                     }
                     else if (msg.type === "resize-by-height") {
-                        node.rescale(msg.heightRatio);
+                        selection.forEach((item) => item.rescale(msg.heightRatio));
                         figma.notify(`⚡️ Hurray! Your selection has been resized by the height.`);
-                    }
-                    else {
-                        figma.closePlugin();
                     }
                 });
             }
         }
         else {
-            figma.notify(`😭 Sorry! You aren't actively selecting a layer.`, { timeout: 100 });
+            figma.notify(`😭 Sorry! You aren't actively selecting a layer.`, { timeout: 200 });
             figma.ui.postMessage({
-                objectWidth: undefined,
-                objectHeight: undefined,
-                percentage: undefined,
+                objectWidth: 0,
+                objectHeight: 0,
+                percentage: 0,
             });
         }
     });
 }
-figma.notify(`Please select a layer to get started.`, { timeout: 2000 });
+if (selection.length === 0) {
+    figma.ui.postMessage({
+        objectWidth: 0,
+        objectHeight: 0,
+        percentage: 0,
+    });
+    // // alert the user on what to do
+    figma.notify(`Please select a layer to get started.`, { timeout: 2000 });
+    figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
+        if (msg.percentage === "0") {
+            figma.notify(`😭 Sorry! You aren't actively selecting a layer.`, { timeout: 200 });
+            figma.ui.postMessage({
+                objectWidth: 0,
+                objectHeight: 0,
+                percentage: 0,
+            });
+        }
+    });
+}
