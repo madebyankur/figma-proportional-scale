79e963962cef4f8ade229f2e928a5f42933b2a16
Ankur Chauhan <ankur.chauhan@auth0.com>
2022-03-26 22:21:51 +0100

    Add alignment toggle and update UI

--- src/code.ts
+++ src/code.ts
@@ -1,8 +1,8 @@
 // show ui in figma
-figma.showUI(__html__, { width: 320, height: 320 });
+figma.showUI(__html__, { width: 336, height: 240 });
 
 // scale function
-const scale = (input: string) => {
+const scale = (input: string, alignment?: string) => {
   // convert string to integer
   const ratio = parseFloat(input);
 
@@ -10,7 +10,58 @@ const scale = (input: string) => {
   for (const node of figma.currentPage.selection) {
     // check if child can be scaled
     if ("rescale" in node) {
+      const currentCenterX = node.width / 2;
+      const centerX = node.x + currentCenterX;
+      const rightX = node.x + node.width;
+      const currentCenterY = node.height / 2;
+      const centerY = node.y + currentCenterY;
+      const rightY = node.y + node.width;
+
       node.rescale(ratio);
+
+      figma.ui.postMessage({
+        objectWidth: node.width | 0,
+        objectHeight: node.height | 0,
+        percentage: 100,
+      });
+
+      const horizontalCenter = node.width / 2;
+      const horizontalRight = node.width;
+      const verticalCenter = node.height / 2;
+      const verticalBottom = node.height;
+
+      switch (alignment) {
+        case "top-left":
+          break;
+        case "top-center":
+          node.x = centerX - horizontalCenter;
+          break;
+        case "top-right":
+          node.x = rightX - horizontalRight;
+          break;
+        case "middle-left":
+          node.y = centerY - verticalCenter;
+          break;
+        case "middle-center":
+          node.y = centerY - verticalCenter;
+          node.x = centerX - horizontalCenter;
+          break;
+        case "middle-right":
+          node.y = centerY - verticalCenter;
+          node.x = rightX - horizontalRight;
+          break;
+        case "bottom-left":
+          node.y = rightY - verticalBottom;
+          break;
+        case "bottom-center":
+          node.y = rightY - verticalBottom;
+          node.x = centerX - horizontalCenter;
+          break;
+        case "bottom-right":
+          node.y = rightY - verticalBottom;
+          node.x = rightX - horizontalRight;
+          break;
+      }
     }
   }
 };
@@ -20,19 +71,19 @@ const checkType = (type, msg) => {
   // scale each node based on percentage input
   switch (type) {
     case "resize-by-percentage":
-      scale(msg.percentageRatio);
+      scale(msg.percentageRatio, msg.buttonType);
       // alert user
       notification();
       break;
     // scale each node based on width input
     case "resize-by-width":
-      scale(msg.widthRatio);
+      scale(msg.widthRatio, msg.buttonType);
       // alert user
       notification();
       break;
     // scale each node based on height input
     case "resize-by-height":
-      scale(msg.heightRatio);
+      scale(msg.heightRatio, msg.buttonType);
       // alert user
       notification();
       break;
